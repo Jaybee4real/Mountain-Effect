18182a853bf02fc57839ce8017337c6b033f6a92
Jaybee4real <okorojijaybee1@gmail.com>
2020-03-20 21:41:38 +0100

    _made cool canvas effect

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
 
     var width, height, largeHeader, canvas, ctx, points, target, animateHeader = true;
 
@@ -9,11 +9,17 @@
 
     function initHeader() {
         width = window.innerWidth;
-        height = window.innerHeight;
-        target = {x: width/2, y: height/2};
+        height = window.innerHeight ;
+        target = { x: width / 2, y: height / 2 };
 
         largeHeader = document.getElementById('large-header');
-        largeHeader.style.height = height+'px';
+        largeHeader.style.height = height + 'px';
+
+
+
+        var background = new Image();
+        background.src = "./assets/img/background.jpg";
+
 
         canvas = document.getElementById('demo-canvas');
         canvas.width = width;
@@ -22,35 +28,35 @@
 
         // create points
         points = [];
-        for(var x = 0; x < width; x = x + width/20) {
-            for(var y = 0; y < height; y = y + height/20) {
-                var px = x + Math.random()*width/20;
-                var py = y + Math.random()*height/20;
-                var p = {x: px, originX: px, y: py, originY: py };
+        for (var x = 0; x < width; x = x + width / 20) {
+            for (var y = 0; y < height; y = y + height / 20) {
+                var px = x + Math.random() * width / 20;
+                var py = y + Math.random() * height / 20;
+                var p = { x: px, originX: px, y: py, originY: py };
                 points.push(p);
             }
         }
 
         // for each point find the 5 closest points
-        for(var i = 0; i < points.length; i++) {
+        for (var i = 0; i < points.length; i++) {
             var closest = [];
             var p1 = points[i];
-            for(var j = 0; j < points.length; j++) {
+            for (var j = 0; j < points.length; j++) {
                 var p2 = points[j]
-                if(!(p1 == p2)) {
+                if (!(p1 == p2)) {
                     var placed = false;
-                    for(var k = 0; k < 5; k++) {
-                        if(!placed) {
-                            if(closest[k] == undefined) {
+                    for (var k = 0; k < 5; k++) {
+                        if (!placed) {
+                            if (closest[k] == undefined) {
                                 closest[k] = p2;
                                 placed = true;
                             }
                         }
                     }
 
-                    for(var k = 0; k < 5; k++) {
-                        if(!placed) {
-                            if(getDistance(p1, p2) < getDistance(p1, closest[k])) {
+                    for (var k = 0; k < 5; k++) {
+                        if (!placed) {
+                            if (getDistance(p1, p2) < getDistance(p1, closest[k])) {
                                 closest[k] = p2;
                                 placed = true;
                             }
@@ -62,15 +68,15 @@
         }
 
         // assign a circle to each point
-        for(var i in points) {
-            var c = new Circle(points[i], 2+Math.random()*2, 'rgba(255,255,255,0.3)');
+        for (var i in points) {
+            var c = new Circle(points[i], 2 + Math.random() * 2, 'rgba(255,255,255,0.9)');
             points[i].circle = c;
         }
     }
 
     // Event handling
     function addListeners() {
-        if(!('ontouchstart' in window)) {
+        if (!('ontouchstart' in window)) {
             window.addEventListener('mousemove', mouseMove);
         }
         window.addEventListener('scroll', scrollCheck);
@@ -83,7 +89,7 @@
             posx = e.pageX;
             posy = e.pageY;
         }
-        else if (e.clientX || e.clientY)    {
+        else if (e.clientX || e.clientY) {
             posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
             posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
         }
@@ -92,14 +98,14 @@
     }
 
     function scrollCheck() {
-        if(document.body.scrollTop > height) animateHeader = false;
+        if (document.body.scrollTop > height) animateHeader = false;
         else animateHeader = true;
     }
 
     function resize() {
         width = window.innerWidth;
         height = window.innerHeight;
-        largeHeader.style.height = height+'px';
+        largeHeader.style.height = height + 'px';
         canvas.width = width;
         canvas.height = height;
     }
@@ -107,23 +113,23 @@
     // animation
     function initAnimation() {
         animate();
-        for(var i in points) {
+        for (var i in points) {
             shiftPoint(points[i]);
         }
     }
 
     function animate() {
-        if(animateHeader) {
-            ctx.clearRect(0,0,width,height);
-            for(var i in points) {
+        if (animateHeader) {
+            ctx.clearRect(0, 0, width, height);
+            for (var i in points) {
                 // detect points in range
-                if(Math.abs(getDistance(target, points[i])) < 4000) {
+                if (Math.abs(getDistance(target, points[i])) < 4000) {
                     points[i].active = 0.3;
                     points[i].circle.active = 0.6;
-                } else if(Math.abs(getDistance(target, points[i])) < 20000) {
+                } else if (Math.abs(getDistance(target, points[i])) < 20000) {
                     points[i].active = 0.1;
                     points[i].circle.active = 0.3;
-                } else if(Math.abs(getDistance(target, points[i])) < 40000) {
+                } else if (Math.abs(getDistance(target, points[i])) < 40000) {
                     points[i].active = 0.02;
                     points[i].circle.active = 0.1;
                 } else {
@@ -139,40 +145,42 @@
     }
 
     function shiftPoint(p) {
-        TweenLite.to(p, 1+1*Math.random(), {x:p.originX-50+Math.random()*100,
-            y: p.originY-50+Math.random()*100, ease:Circ.easeInOut,
-            onComplete: function() {
+        TweenLite.to(p, 1 + 1 * Math.random(), {
+            x: p.originX - 50 + Math.random() * 100,
+            y: p.originY - 50 + Math.random() * 100, ease: Circ.easeInOut,
+            onComplete: function () {
                 shiftPoint(p);
-            }});
+            }
+        });
     }
 
     // Canvas manipulation
     function drawLines(p) {
-        if(!p.active) return;
-        for(var i in p.closest) {
+        if (!p.active) return;
+        for (var i in p.closest) {
             ctx.beginPath();
             ctx.moveTo(p.x, p.y);
             ctx.lineTo(p.closest[i].x, p.closest[i].y);
-            ctx.strokeStyle = 'rgba(156,217,249,'+ p.active+')';
+            ctx.strokeStyle = 'rgba(255,255,255,' + p.active + ')';
             ctx.stroke();
         }
     }
 
-    function Circle(pos,rad,color) {
+    function Circle(pos, rad, color) {
         var _this = this;
 
         // constructor
-        (function() {
+        (function () {
             _this.pos = pos || null;
             _this.radius = rad || null;
             _this.color = color || null;
         })();
 
-        this.draw = function() {
-            if(!_this.active) return;
+        this.draw = function () {
+            if (!_this.active) return;
             ctx.beginPath();
             ctx.arc(_this.pos.x, _this.pos.y, _this.radius, 0, 2 * Math.PI, false);
-            ctx.fillStyle = 'rgba(156,217,249,'+ _this.active+')';
+            ctx.fillStyle = 'rgb(225, 255, 255,' + _this.active + ')';
             ctx.fill();
         };
     }
@@ -181,5 +189,183 @@
     function getDistance(p1, p2) {
         return Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2);
     }
-    
+
+
 })();
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////
+
+var w = c.width = window.innerWidth,
+    h = c.height = window.innerHeight *2,
+    ctx = c.getContext('2d'),
+
+    minDist = 30,
+    maxDist = 400,
+    initialWidth = 5,
+    maxLines = 500,
+    initialLines = 10,
+    speed = 4,
+
+    lines = [],
+    frame = 0,
+    timeSinceLast = 0,
+
+    dirs = [
+        // straight x, y velocity
+        [0, 1],
+        [1, 0],
+        [0, -1],
+        [-1, 0],
+        // diagonals, 0.7 = sin(PI/4) = cos(PI/4)
+        [.7, .7],
+        [.7, -.7],
+        [-.7, .7],
+        [-.7, -.7]
+    ],
+    starter = { // starting parent line, just a pseudo line
+
+        x: w / 2,
+        y: h / 2,
+        vx: 0,
+        vy: 0,
+        width: initialWidth
+    };
+
+function init() {
+
+    lines.length = 0;
+
+    for (var i = 0; i < initialLines; ++i)
+        lines.push(new Line(starter));
+
+    ctx.fillStyle = 'rgba(39, 33, 80, 0.00)';
+    ctx.fillRect(0, 0, w, h);
+
+    ctx.lineCap = 'round';
+}
+function getColor(x) {
+
+    return 'hsl( hue, 80%, 50% )'.replace(
+        'hue', x / w * 360 + frame
+    );
+}
+function anim() {
+
+    window.requestAnimationFrame(anim);
+
+    ++frame;
+
+    ctx.shadowBlur = 0;
+    ctx.fillStyle = 'rgba(39, 33, 80, 0.00)';
+    // ctx.fillRect(0, 0, w, h);
+    ctx.shadowBlur = .5;
+
+    for (var i = 0; i < lines.length; ++i)
+
+        if (lines[i].step()) { // if true it's dead
+
+            lines.splice(i, 1);
+            --i;
+
+        }
+
+    // spawn new
+
+    ++timeSinceLast
+
+    if (lines.length < maxLines && timeSinceLast > 10 && Math.random() < .5) {
+
+        timeSinceLast = 0;
+
+        lines.push(new Line(starter));
+
+        // cover the middle;
+        ctx.fillStyle = ctx.shadowColor = getColor(starter.x);
+        ctx.beginPath();
+        ctx.arc(starter.x, starter.y, initialWidth, 0, Math.PI * 2);
+        ctx.fill();
+    }
+}
+
+function Line(parent) {
+
+    this.x = parent.x | 0;
+    this.y = parent.y | 0;
+    this.width = parent.width / 1.25;
+
+    do {
+
+        var dir = dirs[(Math.random() * dirs.length) | 0];
+        this.vx = dir[0];
+        this.vy = dir[1];
+
+    } while (
+        (this.vx === -parent.vx && this.vy === -parent.vy) || (this.vx === parent.vx && this.vy === parent.vy));
+
+    this.vx *= speed;
+    this.vy *= speed;
+
+    this.dist = (Math.random() * (maxDist - minDist) + minDist);
+
+}
+Line.prototype.step = function () {
+
+    var dead = false;
+
+    var prevX = this.x,
+        prevY = this.y;
+
+    this.x += this.vx;
+    this.y += this.vy;
+
+    --this.dist;
+
+    // kill if out of screen
+    if (this.x < 0 || this.x > w || this.y < 0 || this.y > h)
+        dead = true;
+
+    // make children :D
+    if (this.dist <= 0 && this.width > 1) {
+
+        // keep yo self, sometimes
+        this.dist = Math.random() * (maxDist - minDist) + minDist;
+
+        // add 2 children
+        if (lines.length < maxLines) lines.push(new Line(this));
+        if (lines.length < maxLines && Math.random() < .5) lines.push(new Line(this));
+
+        // kill the poor thing
+        if (Math.random() < .2) dead = true;
+    }
+
+    ctx.strokeStyle = ctx.shadowColor = getColor(this.x);
+    ctx.beginPath();
+    ctx.lineWidth = this.width;
+    ctx.moveTo(this.x, this.y);
+    ctx.lineTo(prevX, prevY);
+    ctx.stroke();
+
+    if (dead) return true
+}
+
+init();
+anim();
+
+window.addEventListener('resize', function () {
+
+    w = c.width = window.innerWidth;
+    h = c.height = window.innerHeight;
+    starter.x = w / 2;
+    starter.y = h / 2;
+
+    init();
+})
+
+
