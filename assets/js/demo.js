fd9ed3d6d4b7b2213bd6e964515e746bbbc78e10
Jaybee4real <okorojijaybee1@gmail.com>
2020-03-21 23:06:53 +0100

    _removed rainbow effect: added header parallax

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -202,175 +202,6 @@
 
 /////////////////////////////////////////////////
 
-var w = c.width = window.innerWidth,
-    h = c.height = window.innerHeight,
-    ctx = c.getContext('2d'),
-
-    minDist = 30,
-    maxDist = 400,
-    initialWidth = 4,
-    maxLines = 15,
-    initialLines = 4,
-    speed = 4,
-
-    lines = [],
-    frame = 0,
-    timeSinceLast = 0,
-
-    dirs = [
-        // straight x, y velocity
-        [0, 1],
-        [1, 0],
-        [0, -1],
-        [-1, 0],
-        // diagonals, 0.7 = sin(PI/4) = cos(PI/4)
-        [.7, .7],
-        [.7, -.7],
-        [-.7, .7],
-        [-.7, -.7]
-    ],
-    starter = { // starting parent line, just a pseudo line
-
-        x: w / 2,
-        y: h / 2,
-        vx: 0,
-        vy: 0,
-        width: initialWidth
-    };
-
-function init() {
-
-    lines.length = 0;
-
-    for (var i = 0; i < initialLines; ++i)
-        lines.push(new Line(starter));
-
-    ctx.fillStyle = 'rgba(39, 33, 80, 0.00)';
-    ctx.fillRect(0, 0, w, h);
-
-    ctx.lineCap = 'round';
-}
-function getColor(x) {
-
-    return 'hsl( hue, 80%, 50% )'.replace(
-        'hue', x / w * 360 + frame
-    );
-}
-function anim() {
-
-    window.requestAnimationFrame(anim);
-
-    ++frame;
-
-    ctx.shadowBlur = 0;
-    ctx.fillStyle = 'rgba(39, 33, 80, 0.00)';
-    // ctx.fillRect(0, 0, w, h);
-    ctx.shadowBlur = .5;
-
-    for (var i = 0; i < lines.length; ++i)
-
-        if (lines[i].step()) { // if true it's dead
-
-            lines.splice(i, 1);
-            --i;
-
-        }
-
-    // spawn new
-
-    ++timeSinceLast
-
-    if (lines.length < maxLines && timeSinceLast > 10 && Math.random() < .5) {
-
-        timeSinceLast = 0;
-
-        lines.push(new Line(starter));
-
-        // cover the middle;
-        ctx.fillStyle = ctx.shadowColor = getColor(starter.x);
-        ctx.beginPath();
-        ctx.arc(starter.x, starter.y, initialWidth, 0, Math.PI * 2);
-        ctx.fill();
-    }
-}
-
-function Line(parent) {
-
-    this.x = parent.x | 0;
-    this.y = parent.y | 0;
-    this.width = parent.width / 1.25;
-
-    do {
-
-        var dir = dirs[(Math.random() * dirs.length) | 0];
-        this.vx = dir[0];
-        this.vy = dir[1];
-
-    } while (
-        (this.vx === -parent.vx && this.vy === -parent.vy) || (this.vx === parent.vx && this.vy === parent.vy));
-
-    this.vx *= speed;
-    this.vy *= speed;
-
-    this.dist = (Math.random() * (maxDist - minDist) + minDist);
-
-}
-Line.prototype.step = function () {
-
-    var dead = false;
-
-    var prevX = this.x,
-        prevY = this.y;
-
-    this.x += this.vx;
-    this.y += this.vy;
-
-    --this.dist;
-
-    // kill if out of screen
-    if (this.x < 0 || this.x > w || this.y < 0 || this.y > h)
-        dead = true;
-
-    // make children :D
-    if (this.dist <= 0 && this.width > 1) {
-
-        // keep yo self, sometimes
-        this.dist = Math.random() * (maxDist - minDist) + minDist;
-
-        // add 2 children
-        if (lines.length < maxLines) lines.push(new Line(this));
-        if (lines.length < maxLines && Math.random() < .5) lines.push(new Line(this));
-
-        // kill the poor thing
-        if (Math.random() < .2) dead = true;
-        if (lines.length > maxLines) lines.pop(lines.length/2)
-    }
-
-    ctx.strokeStyle = ctx.shadowColor = getColor(this.x);
-    ctx.beginPath();
-    ctx.lineWidth = this.width;
-    ctx.moveTo(this.x, this.y);
-    ctx.lineTo(prevX, prevY);
-    ctx.stroke();
-
-    if (dead) return true
-}
-
-init();
-anim();
-
-window.addEventListener('resize', function () {
-
-    w = c.width = window.innerWidth;
-    h = c.height = window.innerHeight;
-    starter.x = w / 2;
-    starter.y = h / 2;
-
-    init();
-})
-
-
-
 
 
 ////////////////////Conrol Navigation////////////////
@@ -387,3 +218,39 @@ window.addEventListener('scroll', function() {
   function navigateDown(){
     
   }
+
+  var layer1 = document.getElementById('layer1')
+  scroll = window.pageYOffset;
+  document.addEventListener("scroll", function(e){
+      var offset = window.pageYOffset;
+      scroll = offset;
+      layer1.style.width = (90 + scroll/3 ) + "%"
+  })
+
+    var layer2 = document.getElementById('layer2')
+  scroll = window.pageYOffset;
+  document.addEventListener("scroll", function(e){
+      var offset = window.pageYOffset;
+      scroll = offset;
+      layer2.style.width = (90 + scroll/3 ) + "%"
+      layer2.style.left =  scroll/50 + "%"
+      layer2.style.transform = "translateX(10%)"
+  })
+
+  var text = document.getElementById('text')
+  var zoom = document.querySelector(".zoom")
+  scroll = window.pageYOffset;
+  document.addEventListener("scroll", function(e){
+      var offset = window.pageYOffset;
+      scroll = offset;
+
+      if(scroll > 20){ scroll = 200
+      text.style.bottom = scroll/6 + "%";
+      text.style.opacity = .1 + scroll/100;
+      zoom.style.backgroundColor = "grey"
+      }
+      else{
+        zoom.style.backgroundColor = "transparent";
+        text.style.opacity = .1
+      }
+  })
