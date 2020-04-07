e568cb2552ef90aa44be55ce279a92eda7ae0eee
Jaybee4real <okorojijaybee1@gmail.com>
2020-04-08 00:19:45 +0100

    _fixing snow bug

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -208,18 +208,18 @@ scroll = window.pageYOffset;
 document.addEventListener("scroll", function (e) {
 	var offset = window.pageYOffset;
 	scroll = offset;
-      layer1.style.width = (90 + scroll/3 ) + "%"
-  })
+	layer1.style.width = (90 + scroll / 3) + "%"
+})
 
 var layer2 = document.getElementById('layer2')
 scroll = window.pageYOffset;
 document.addEventListener("scroll", function (e) {
 	var offset = window.pageYOffset;
 	scroll = offset;
-      layer2.style.width = (90 + scroll/3 ) + "%"
-      layer2.style.left =  scroll/50 + "%"
-      layer2.style.transform = "translateX(10%)"
-  })
+	layer2.style.width = (90 + scroll / 3) + "%"
+	layer2.style.left = scroll / 50 + "%"
+	layer2.style.transform = "translateX(10%)"
+})
 
 
 
@@ -258,113 +258,18 @@ document.addEventListener("scroll", function (e) {
 })
 
 
-particlesJS("particles", {
-	"particles": {
-	  "number": {
-		"value": 400,
-		"density": {
-		  "enable": true,
-		  "value_area": 800
-		}
-	  },
-	  "color": {
-		"value": "#ffffff"
-	  },
-	  "shape": {
-		"type": "image",
-		"stroke": {
-		  "width": 5,
-		  "color": "#fff"
-		},
-		"polygon": {
-		  "nb_sides": 5
-		},
-		"image": {
-		  "src": "../assets/img/snow.png",
-		  "width": 100,
-		  "height": 100
-		}
-	  },
-	  "opacity": {
-		"value": 0.7,
-		"random": false,
-		"anim": {
-		  "enable": false,
-		  "speed": .5,
-		  "opacity_min": 0.1,
-		  "sync": false
-		}
-	  },
-	  "size": {
-		"value": 9,
-		"random": true,
-		"anim": {
-		  "enable": false,
-		  "speed": 10,
-		  "size_min": 0.1,
-		  "sync": false
-		}
-	  },
-	  "line_linked": {
-		"enable": false,
-		"distance": 50,
-		"color": "#ffffff",
-		"opacity": 0.6,
-		"width": 1
-	  },
-	  "move": {
-		"enable": true,
-		"speed": 2,
-		"direction": "bottom",
-		"random": true,
-		"straight": false,
-		"out_mode": "out",
-		"bounce": false,
-		"attract": {
-		  "enable": true,
-		  "rotateX": 300,
-		  "rotateY": 1200
-		}
-	  }
-	},
-	"interactivity": {
-	  "detect_on": "none",
-	  "events": {
-		"onhover": {
-		  "enable": false,
-		  "mode":  "bubble"
-		},
-		"onclick": {
-		  "enable": true,
-		  "mode": "repulse"
-		},
-		"resize": true
-	  },
-	  "modes": {
-		"grab": {
-		  "distance": 150,
-		  "line_linked": {
-			"opacity": 1
-		  }
-		},
-		"bubble": {
-		  "distance": 200,
-		  "size": 40,
-		  "duration": 2,
-		  "opacity": 8,
-		  "speed": 3
-		},
-		"repulse": {
-		  "distance": 200,
-		  "duration": 0.2
-		},
-		"push": {
-		  "particles_nb": 4
-		},
-		"remove": {
-		  "particles_nb": 2
-		}
-	  }
-	},
-	"retina_detect": true
-  });
+let hamburger = document.querySelector(".hamburger-icon")
+let sideMenu = document.querySelector(".side-menu")
+var color = window.getComputedStyle(
+	document.querySelector('.side-menu'), '::before'
+)
+
+
+
+hamburger.addEventListener("click", () => {
+	hamburger.classList.toggle("active");
+	sideMenu.classList.toggle("active");
+
+})
+
+
